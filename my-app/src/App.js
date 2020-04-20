0b4197dbe91af738ef8a326e08ba42cf95b5e9a3
hellosophiee <glitchfishzero@gmail.com>
2020-04-20 23:29:41 +0500

    feat: asynchronous requests

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react'; // подключение библиотеки React
 // испорт пакета prop-types удален, так как в этом файле prop-types не исп-ся
-import { Add } from './components/Add'; // ./ = текущая директория,
-import { News } from './components/News'; // далее мы идем в директорию components и в нужный компонент
-import newsData from './data/newsData.json' // импорт по дефолту
+import { Add } from './components/Add'; // ./ = текущая директория
+import { News } from './components/News';
+// удален импорт newsData
 import './App.css'; // подключение файла стилей
 
 class App extends React.Component {
   state = {
-    news: newsData,
+    news: null, // было newsData
+    isLoading: false, // статус для манипуляций "прелоадером" ("Загружаю..." в нашем с лучае)
   };
   handleAddNews = data => {
     // сначала мы формируем массив, на основе всего того, что уже было в новостях и кладем это все в новый массив + новую новость кладем в начало массива
@@ -16,14 +17,32 @@ class App extends React.Component {
     // затем обновляем новый массив новостей в this.state.news
     this.setState({ news: nextNews });
   };
+  componentDidMount() {
+    // ставим isLoading true, т.е. запрос за данными начался
+    // фактически он начнется в строке с fetch, но на переход от одной строки к другой пройдут миллисекунды
+    this.setState({ isLoading: true })
+    fetch('http://localhost:3000/data/newsData.json')
+    .then(response => {
+      return response.json()
+    })
+    .then(data => {
+      setTimeout(() => {
+        // запрос завершился успешно, делаем isLoading: false
+      // в news кладем пришедшие данные
+      this.setState({ isLoading: false, news: data })
+      }, 3000) // добавили задержку в 3 секунды
+    })
+  }
 
   render() {
+    const { news, isLoading } = this.state // все необходимое взяли из state 
+
     return (
       <React.Fragment>
         <Add onAddNews={this.handleAddNews} />
         <h3>Новости</h3>
-        {/* считали новости из this.state */}
-        <News data={this.state.news} />
+        {isLoading && <p>Загружаю...</p>}
+        {Array.isArray(news) && <News data={news} />}
       </React.Fragment>
     );
   }
