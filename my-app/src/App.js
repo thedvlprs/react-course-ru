0fe4720bb03f61d0a832bdce278df1a8e4272a32
hellosophiee <glitchfishzero@gmail.com>
2020-04-21 15:01:14 +0500

    feat: refactoring

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -10,30 +10,50 @@ class App extends React.Component {
     news: null, // было newsData
     isLoading: false, // статус для манипуляций "прелоадером" ("Загружаю..." в нашем с лучае)
   };
-  handleAddNews = data => {
-    // сначала мы формируем массив, на основе всего того, что уже было в новостях и кладем это все в новый массив + новую новость кладем в начало массива
-    const nextNews = [data, ...this.state.news];
+  static getDirevedStateFromProps(props, state) {
+    let nextFilteredNews
 
-    // затем обновляем новый массив новостей в this.state.news
-    this.setState({ news: nextNews });
-  };
+    // смотрим в state.news (ранее смотрели в props)
+    // и проверяем, чтобы не клонировать null
+    // например, в момент первой отрисовки
+    if (Array.isArray(state.news)) {
+      nextFilteredNews = [...state.news]
+
+      nextFilteredNews.forEach((item, index) => {
+        if (item.bigText.toLowerCase().indexOf('pubg') !== -1) {
+          item.bigText = 'СПАМ'
+        }
+      })
+
+      return {
+        filteredNews: nextFilteredNews,
+      }
+    }
+
+    return null
+  }
   componentDidMount() {
     // ставим isLoading true, т.е. запрос за данными начался
     // фактически он начнется в строке с fetch, но на переход от одной строки к другой пройдут миллисекунды
     this.setState({ isLoading: true })
-    fetch('http://localhost:3000/data/newsData.json')
-    .then(response => {
-      return response.json()
-    })
-    .then(data => {
-      setTimeout(() => {
-        // запрос завершился успешно, делаем isLoading: false
-      // в news кладем пришедшие данные
-      this.setState({ isLoading: false, news: data })
-      }, 3000) // добавили задержку в 3 секундыы
-    })
+    fetch('https://react-course-ru.now.sh/data/newsData.json')
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setTimeout(() => {
+          // запрос завершился успешно, делаем isLoading: false
+          // в news кладем пришедшие данные
+          this.setState({ isLoading: false, news: data });
+        }, 1000); // изменили таймер на 1000, чтобы не ждать долго
+      });
   }
 
+  handleAddNews = data => {
+    const nextNews = [data, ...this.state.news]
+    this.setState({ news: nextNews })
+  }
+ 
   render() {
     const { news, isLoading } = this.state // все необходимое взяли из state 
 
